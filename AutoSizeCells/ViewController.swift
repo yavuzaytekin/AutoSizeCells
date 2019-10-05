d7a2a4c53462c8fe2b414ca271a731b127d19748
yavuzaytekin <yavuzaytekin219@gmail.com>
2019-10-05 18:24:45 +0300

    Initial commit

---
+++ AutoSizeCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoSizeCells
+//
+//  Created by Yavuz on 5.10.2019.
+//  Copyright © 2019 Yavuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
